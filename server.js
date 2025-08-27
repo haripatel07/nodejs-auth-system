50fd5bddd975c8dbfc52c2c5c2f6886dedeaf70e
Hari Patel <patelhariv18@gmail.com>
2025-08-27 20:17:31 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const { notFound, errorHandler } = require('./middleware/errorHandler');
+
+// Connect to database
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-// Define a simple route
-app.get('/', (req, res) => {
-    res.send('Server is running');
-})
+// Routes
+app.use('/api/auth', authRoutes);
 
-// Listen on the specified port
+// Error Handling Middleware
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
